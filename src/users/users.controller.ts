6ee2dd2632c948759076085a8b8a30ca7b57d0c2
Yousuf-Sh <ys937721@gmail.com>
2025-06-03 15:04:48 +0500

    Add user Service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { get } from 'http';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    allUsers(@Query('role') role: 'USER'|'ADMIN'){
+        if(!role){
+            return 'All users';
+        }
+        return 'user with role '+role 
+    }
+    @Get(':id')
+    showUser(@Param('id') id: string){
+        return `User with id : ${id}`;
+    }
+    @Post()
+    createUser(@Body() user:{}){
+        return user;
+    }
+    @Patch(':id')
+    updateUser(@Param('id') id:string, @Body() userUpdate:{}){
+        return {id, ...userUpdate};
+    }
+    @Delete(':id')
+    deleteUser(@Param('id') id:string){
+        return `Deleted user with id ${id}`;
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+     private users = [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "USER",
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "USER",
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "STAFF",
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "USER",
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN",
+        }
+    ];
+
+    getAll(role?: 'USER' | 'STAFF' | 'ADMIN'){
+        if(!role){
+            return this.users;
+        }
+        return this.users.filter(
+            user => user.role === role
+        );
+    }
+    show(id:number){
+        if(!id){
+            throw new HttpException('Not Found',HttpStatus.NOT_FOUND);
+        }
+        return this.users.filter(
+            user => user.id===id
+        );
+    }
+    store(user:{ name:string,email:string,role: 'USER' | 'STAFF' | 'ADMIN'}){
+    const byBiggestId = [...this.users].sort(
+        (a,b)=> b.id - a.id
+    );
+    const newUser = {
+            id : byBiggestId[0].id+1,
+            name : user.name,
+            email : user.email,
+            role : user.role
+        }
+        this.users.push(newUser);
+        return newUser;      
+    }
+    update(id:number, updateUser:{ name:string,email:string,role: 'USER' | 'STAFF' | 'ADMIN'}){
+        this.users = this.users.map(user=>{
+            if(user.id === id){
+                return {...user,...updateUser}
+            }
+            return user
+        })
+        return this.show(id)   
+    }
+    delete(id:number){
+        const deletedUser = this.show(id);
+        this.users = this.users.filter(user => user.id != id);
+        return deletedUser;
+    }
+}
