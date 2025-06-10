963917c3f633e77ec88af8611297f797969e7849
Yousuf-Sh <ys937721@gmail.com>
2025-06-10 14:35:32 +0500

    Add validation to Posts and Tags Post Methods

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,8 +1,24 @@
+import { ArrayMinSize, ArrayNotEmpty, IsArray, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 
 export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
   title:string;
+  
+  @IsNotEmpty()
+  @IsString()
   content:string;
+  
+  @IsNotEmpty()
+  @IsNumber()
   userId:number;  
+  
+  @IsArray()
+  @ArrayNotEmpty()
+  @ArrayMinSize(1)
+  @ArrayNotEmpty()
+  @IsNumber({allowNaN:false,allowInfinity:false},{each:true})
+  @IsNotEmpty()
   tags:number[];
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, ValidationPipe } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
@@ -8,7 +8,7 @@ export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
+  create(@Body(ValidationPipe) createPostDto: CreatePostDto) {
     // return createPostDto; 
     return this.postsService.create(createPostDto);
   }
